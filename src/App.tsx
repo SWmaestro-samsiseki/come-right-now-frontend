398b53d2e04272c8e81ba336a9c7ccf5ab94f504
JaeHyeok-Han <hjh0633@gmail.com>
2022-07-17 18:05:22 +0900

    Feat: main 페이지 라우터 설정 (#WTD-123) 메인 페이지 라우터 설정 및 기본 페이지 구현해서 테스트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/main" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
