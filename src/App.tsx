ef3eb4519011f00139700e597692ea0496febaef
JaeHyeok-Han <hjh0633@gmail.com>
2022-07-19 16:46:53 +0900

    Feat: 루트 및 로그인 라우터 구현 / 와 /login 라우터를 구현하고 /접속시 /login으로 redirect시킬 수 있도록 함

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import RequestPage from './pages/RequestPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace={true} />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/request" element={<RequestPage />} />
         </Routes>
