4400fc0e2d9fbbc756ba8e92452f47d64f63081b
JaeHyeok-Han <hjh0633@gmail.com>
2022-08-06 16:29:13 +0900

    Feat: 유저 SectionMain 구현
    - UserSection의 일부인 main 구현
    - main에 실시간 예약 버튼 구현

--- src/components/user/UserSection.tsx
+++ src/components/user/UserSection.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import SectionMain from '../../pages/user/SectionMain';
 
 const SectionContainer = styled.section`
   width: 100%;
@@ -10,7 +11,7 @@ function UserSection() {
   return (
     <SectionContainer>
       <Routes>
-        <Route path="" element={<div>메인</div>} />
+        <Route path="" element={<SectionMain />} />
         <Route path="star" element={<div>찜</div>} />
         <Route path="history" element={<div>이용내역</div>} />
         <Route path="mypage" element={<div>마이페이지</div>} />
---
+++ src/pages/user/SectionMain.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 100%;
+`;
+const RequestBtn = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 48px;
+  background: #54c2ff;
+  border-radius: 4px;
+  font: normal 700 14px / 20px 'IBM Plex Sans KR';
+  color: white;
+  text-decoration: none;
+`;
+function SectionMain() {
+  return (
+    <MainContainer>
+      <RequestBtn to="/request" replace={true}>
+        + 실시간 예약
+      </RequestBtn>
+    </MainContainer>
+  );
+}
+
+export default SectionMain;
