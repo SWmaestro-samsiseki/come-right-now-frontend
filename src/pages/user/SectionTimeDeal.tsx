084750fe9a530fd3b229fad6e35187d7804095a8
JaeHyeok-Han <hjh0633@gmail.com>
2022-10-15 16:29:54 +0900

    Feat: 이용자측 타임딜탭에 들어가면 타임딜 아이템 fetch

--- src/pages/user/SectionTimeDeal.tsx
+++ src/pages/user/SectionTimeDeal.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import useTimeDealStore from '../../stores/user/timeDealStore';
 import MenuHeader from '../../components/user/MenuHeader';
 import ItemTimeDeal from '../../components/user/ItemTimeDeal';
+import { getTimeDealByUser } from '../../utils/timeDeal';
+import { useEffect } from 'react';
+import useRequestInfoStore from '../../stores/user/requestInfoStore';
 
 const Container = styled.div`
   position: relative;
@@ -32,7 +35,23 @@ const ListContainer = styled.div`
 `;
 
 function SectionTimeDeal() {
-  const { timeDealList } = useTimeDealStore();
+  const { latitude, longitude } = useRequestInfoStore();
+  const { timeDealList, initTimeDeal } = useTimeDealStore();
+
+  async function fetchTimeDeal(latitude: number, longitude: number) {
+    const response = await getTimeDealByUser(latitude, longitude);
+    if (!('error' in response)) {
+      initTimeDeal(response);
+    } else {
+      console.log(response.message);
+    }
+  }
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      fetchTimeDeal(latitude, longitude);
+    }
+  }, [latitude, longitude]);
 
   return (
     <Container>
